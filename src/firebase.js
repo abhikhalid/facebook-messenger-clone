148e57e9ad394de99b68c4ba507ae5898b02dca4
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2022-04-10 21:12:22 +0600

    second-commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwYhD8dXV-m2b7JmT7Pj-1IzHl1aIRYq8",
+    authDomain: "facebook-messenger-clone-86a7b.firebaseapp.com",
+    projectId: "facebook-messenger-clone-86a7b",
+    storageBucket: "facebook-messenger-clone-86a7b.appspot.com",
+    messagingSenderId: "432018710387",
+    appId: "1:432018710387:web:7c34b74691b3c8efeb6f06"
+  };
